03db0aa378fa820119433058a680aee874afcf80
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-06-08 19:43:05 +0200

    add(compose) 1 document(record) in database and fetch documents andupdate homepage

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const app = express();
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+const databaseName = "/blogDB"
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useNewUrlParser', true);
+mongoose.connect('mongodb://localhost' + databaseName);
+
+const blogSchema = new mongoose.Schema({
+  title: String,
+  text: String
+})
+const BlogPostModel = mongoose.model("Blog",blogSchema);
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -13,11 +24,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+
+
 const posts = []
 
 app.get("/", function (req, res) {
   // Render list.ejs template with this variable(marker)
-  res.render("home", {homeSampleContent: homeStartingContent, postsInHome:posts});
+  BlogPostModel.find({}, function (err, allBlogPosts) {
+    console.log(allBlogPosts);
+    res.render("home", {homeSampleContent: homeStartingContent, postsInHome:allBlogPosts});
+  })
+
 })
 
 app.get("/about", function (req, res) {
@@ -32,11 +49,17 @@ app.get("/compose", function (req, res) {
   res.render("compose.ejs");
 })
 
+
 app.post("/compose", function (req, res) {
   const post = {
     postTitle: req.body.postTitle,
     postBody: req.body.postBody
   }
+  const blogPost = new BlogPostModel({
+    title: req.body.postTitle,
+    text: req.body.postBody
+  })
+  blogPost.save();
   posts.push(post);
   res.redirect("/");
 })
