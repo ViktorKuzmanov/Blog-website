a47692e9ebd3e4e60040e9974da0db049ee04967
Viktor Kuzmanov <kuzmanovviktor4@gmail.com>
2020-06-09 14:44:36 +0200

    make read more button work on every blog post

--- app.js
+++ app.js
@@ -16,9 +16,9 @@ const blogSchema = new mongoose.Schema({
 })
 const BlogPostModel = mongoose.model("Blog",blogSchema);
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "This is an example  post. Go to /compose to write a new Blog Post";
+const aboutContent = "This is an example about page";
+const contactContent = "This is an example contact page";
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -69,18 +69,18 @@ app.post("/compose", function (req, res) {
 
 app.get("/posts/:postName", function (req, res) {
   const requestedTitle = _.lowerCase(req.params.postName);
-  posts.forEach((post, i) => {
-    const storedPostTitle = _.lowerCase(post.postTitle);
-    if(storedPostTitle === requestedTitle) {
-      res.render("post.ejs", {postToShow:post});
+
+  BlogPostModel.findOne({title: requestedTitle}, function (err, foundBlogPost) {
+    if(err) {res.send("error 404, post not found");}
+    else {
+        res.render("post.ejs", {postToShow: foundBlogPost});
     }
-  });
-  res.send("error 404, post not found");
+  })
 })
 
-app.get("/posts/:postName", function (req, res) {
-  res.send("ok");
-})
+// app.get("/posts/:postName", function (req, res) {
+//   res.send("ok");
+// })
 
 app.listen(3000, function (req, res) {
   console.log("Server started running on port 3000...");
